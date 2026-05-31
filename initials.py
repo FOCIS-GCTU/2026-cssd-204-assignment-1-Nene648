f40dd1347b6fe90298fb4ff09f3db71b16db5ba6
Nene Baah Nomo-Tetteh <2425400687@live.gctu.edu.gh>
2026-05-31 18:38:24 +0000

    Complete Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Nene Baah Nomo-Tetteh
+# STUDENT ID:  2425400687
+# Email: 2425400687@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Nene Baah Nomo-Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
 
+def main(): #This is the main function
+ 
+    print()
+    print("...NBNT")
+    print()
+    print(
+        """
+...NN........NN........BBBBB...............NN........NN........TTTTTTTTTTTT.....
+...NNN.......NN........BBBBBBBBBB..........NNN.......NN........TTTTTTTTTTTT.....
+...NNNN......NN........BB.......BBB........NNNN......NN............TTTT.........
+...NN..NN....NN........BB.......BB.........NN..NN....NN............TTTT.........
+...NN...NN...NN........BBBBBBBB............NN...NN...NN............TTTT.........
+...NN....NN..NN........BBBBBBBB............NN....NN..NN............TTTT.........
+...NN.....NN.NN........BB.......BB.........NN.....NN.NN............TTTT.........
+...NN......NNNN........BB.......BBB........NN......NNNN............TTTT.........
+...NN.......NNN...**...BBBBBBBBBB.....**...NN.......NNN...**.......TTTT.......**
+...NN........NN...**...BBBBBB.........**...NN........NN...**.......TTTT.......**
+"""
+    )
+    print()
 
-main()
+
+main() #Calling the main function
